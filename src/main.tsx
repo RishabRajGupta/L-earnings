21db7873e54c704008c5a2e75aac7229d464eab9
Rishab Raj Gupta <rishabraj182003@gmail.com>
2025-11-19 03:51:21 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,20 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// AWS Amplify
 import { Amplify } from "aws-amplify";
 import awsconfig from "./aws-exports";
+
+// Auth Provider (VERY IMPORTANT)
+import { AuthProvider } from "./contexts/AuthContext";
 
 // Initialize Amplify
 Amplify.configure(awsconfig);
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
