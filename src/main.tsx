67bb97885a7c23e6f6bcb355be543d1bd832f5e1
Rishab Raj Gupta <rishabraj182003@gmail.com>
2025-11-19 01:00:31 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,16 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
+
+// Initialize AWS Cognito
+Amplify.configure(awsconfig);
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
