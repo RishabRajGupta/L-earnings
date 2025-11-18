f0a3e6d053ae8625a701a8f82370b573a6548265
Rishab Raj Gupta <rishabraj182003@gmail.com>
2025-11-19 04:21:23 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import Courses from "./pages/Courses";
 import CourseDetail from "./pages/CourseDetail";
@@ -17,9 +17,10 @@ import FinalTest from "./pages/FinalTest";
 import Dashboard from "./pages/Dashboard";
 import Profile from "./pages/Profile";
 import ChatBot from "./components/ChatBot";
-import { AuthProvider } from "./contexts/AuthContext";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Verify from "./pages/Verify";
+import Navbar from "./components/Navbar"; 
+// IMPORTANT: Navbar was missing in your App.tsx, make sure it's imported
 
 const queryClient = new QueryClient();
 
@@ -27,28 +28,27 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <ThemeProvider>
-        <AuthProvider>
-          <TooltipProvider>
-            <Toaster />
-            <Sonner />
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/courses" element={<Courses />} />
-              <Route path="/courses/:id" element={<CourseDetail />} />
-              <Route path="/courses/:id/test" element={<FinalTest />} />
-              <Route path="/how-it-works" element={<HowItWorksPage />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/my-courses" element={<MyCourses />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/verify" element={<Verify />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-            <ChatBot />
-          </TooltipProvider>
-        </AuthProvider>
+        <TooltipProvider>
+          <Navbar />   {/* Add this if not already globally shown */}
+          <Toaster />
+          <Sonner />
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/courses" element={<Courses />} />
+            <Route path="/courses/:id" element={<CourseDetail />} />
+            <Route path="/courses/:id/test" element={<FinalTest />} />
+            <Route path="/how-it-works" element={<HowItWorksPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/my-courses" element={<MyCourses />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/verify" element={<Verify />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <ChatBot />
+        </TooltipProvider>
       </ThemeProvider>
     </BrowserRouter>
   </QueryClientProvider>
