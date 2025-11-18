b17efd3c7b4c16f3ab3fc7c7af1ad4f730d0fb49
Rishab Raj Gupta <rishabraj182003@gmail.com>
2025-11-19 00:59:25 +0530

    Create aws-exports.js

---
+++ src/aws-exports.js
@@ -0,0 +1,9 @@
+const awsconfig = {
+  Auth: {
+    region: "us-east-1",
+    userPoolId: "us-east-1_z2tH7rL2Q",
+    userPoolWebClientId: "vir2ppe6l7dkvvs2i4js044bj",
+  }
+};
+
+export default awsconfig;
