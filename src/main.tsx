7bc110fdff81c2075dc776e5524781181121c122
Rishab Raj Gupta <rishabraj182003@gmail.com>
2025-11-19 01:58:05 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-import { Amplify } from "aws-amplify";   // <-- FIXED
+import { Amplify } from "aws-amplify";
 import awsconfig from "./aws-exports";
 
-// Initialize Amplify
 Amplify.configure(awsconfig);
 
 createRoot(document.getElementById("root")!).render(
